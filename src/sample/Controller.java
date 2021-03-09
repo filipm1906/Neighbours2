928f3e123cc036db1cac3788f77c7c0bd60452d5
Wiktor Przygodzki <przygodzkiwiktor@gmail.com>
2021-03-09 19:02:06 +0100

    WiktorSprzataPoSobie

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -22,10 +22,10 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    }
+    public Button btnOK;
+    public ChoiceBox metryka;
+    public ChoiceBox palametrK;
 
     public List<List<String>> wczytajDane(ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
@@ -33,29 +33,26 @@ public class Controller implements Initializable {
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("Text Files", "*.csv"));
         File selectedFile = fileChooser.showOpenDialog(null);
-            List<List<String>> pacjenci = new ArrayList<>();
-            try (BufferedReader br = new BufferedReader(new FileReader(selectedFile))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    String[] values = line.split(";");
-                    pacjenci.add(Arrays.asList(values));
-                }
-                pacjenci.get(0).set(0, pacjenci.get(0).get(0).substring(1));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+        List<List<String>> pacjenci = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(selectedFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                pacjenci.add(Arrays.asList(values));
             }
+            pacjenci.get(0).set(0, pacjenci.get(0).get(0).substring(1));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return pacjenci;
-
-    public Button btnOK;
-    public ChoiceBox metryka;
-    public ChoiceBox palametrK;
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        metryka.getItems().add("Manhattan");
-        metryka.getItems().add("Euklidesowa");
     }
-}
+
+
+        @Override
+        public void initialize (URL url, ResourceBundle resourceBundle){
+            metryka.getItems().add("Manhattan");
+            metryka.getItems().add("Euklidesowa");
+        }
+    }
